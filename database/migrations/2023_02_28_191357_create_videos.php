7ea4e8c55667436289a3fc63cd630c5126382a5d
msholl <matheus@conetca-soft.com.br>
2023-03-01 22:10:41 -0300

    criando tabela de videos

---
+++ database/migrations/2023_02_28_191357_create_videos.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('titulo', 128);
+            $table->string('descricao', 128);
+            $table->string('url', 128);
+            $table->integer('categoriaId');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('videos');
+    }
+};
